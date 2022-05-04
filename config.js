8700939d5b01d34d0beffc4821aeb30052f96685
deltaplanoo <pietro.ciani01@gmail.com>
2022-05-04 23:00:41 +0200

    weather API integration added

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '999cbaafe4df92cfe6f6e1ec2d4eb7b3', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
