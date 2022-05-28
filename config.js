96d7df886e0b9ac6abb900745cf2eb46502d2fd4
PietroCiani <pietro.ciani@stud.unifi.it>
2022-05-28 10:05:05 +0200

    added onedrive

--- config.js
+++ config.js
@@ -161,8 +161,8 @@ const CONFIG = {
 			id: '2',
 			links: [
 				{
-					name: 'Inspirational',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+					name: 'OneDrive',
+					link: 'https://www.https://onedrive.live.com/?id=1FEFA23C0BFB89FC%2134594&cid=1FEFA23C0BFB89FC.com/watch?v=dQw4w9WgXcQ',
 				},
 				{
 					name: 'Classic',
